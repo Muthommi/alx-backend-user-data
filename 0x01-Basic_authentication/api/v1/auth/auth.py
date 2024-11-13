6d5c3ed418d5e0f843001b45213a589f6e2f3679
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-13 08:03:59 +0000

    Define the routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,17 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if authentication is required.
-        Returns False.
+        Returns true if path is none or not in excluded_paths.
         """
-        return False
+        if path is None or not excluded_paths:
+            return True
+
+        path = path.rstrip('/')
+        for ex_path in excluded_paths:
+            if ex_path.rstrip('/') == path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
