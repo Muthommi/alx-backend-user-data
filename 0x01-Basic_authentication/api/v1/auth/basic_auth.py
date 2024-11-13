928fad7143ad741432a2a23c7140d3dd631eea10
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-13 09:45:58 +0000

    User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,17 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Method to extract user email and password
+        Returns: tuple
+        """
+        if (decoded_base64_authorization_header is None or
+                not isinstance(decoded_base64_authorization_header, str)):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_password = (
+                decoded_base64_authorization_header.split(':', 1))
+        return user_email, user_password
